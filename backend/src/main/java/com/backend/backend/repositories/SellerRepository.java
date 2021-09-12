590a8e2ed2e9a07a008451a361036f9cd0fee304
Pedro-lms <pedrodaluzmachado@gmail.com>
2021-09-11 22:47:26 -0300

    Camadas

---
+++ backend/src/main/java/com/backend/backend/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.backend.backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.backend.backend.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
